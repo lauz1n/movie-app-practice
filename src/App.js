2ce0339c3e1b26e961254abd6c486d215b4d4645
Samuel <samuelcvmoura@gmail.com>
2022-09-21 18:09:53 -0300

    https changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css"
 import SearchIcon from "./search.svg"
 import MovieCard from "./MovieCard"
 
-const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_API_URL}`
+const API_URL = `https://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_API_URL}`
 
 const App = () => {
   const [movies, setMovies] = useState([])
